81465ec1ef30f16ca66ce19f03a3579f9a6ffd11
pekarnick <guillerohde@gmail.com>
2020-06-15 20:06:59 -0300

    CMSCake 4 - Inicial

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
  * CakePHP(tm) : Rapid Development Framework (https://cakephp.org)
  * Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
  *
- * Licensed under The MIT License
+ * Licensed under The MIT Lcicense
  * For full copyright and license information, please see the LICENSE.txt
  * Redistributions of files must retain the above copyright notice.
  *
